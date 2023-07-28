abf5aac6fbade1f3454e85345cad9087f694ea56
pnttai <139779096+pnttai@users.noreply.github.com>
2023-07-28 20:01:14 +0700

    Add files via upload

---
+++ src/AddProductForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import './App.css';
+import {nanoid} from 'nanoid';
+function AddProductForm  ({setDatas}){
+  const [newTitle,setNewTitle] = useState("");
+    const [newPrice, setNewPrice] = useState(20000);
+    const [newQuantity, setNewQuantity] = useState(1);
+  function handleSubmit(e){
+    e.preventDefault();
+    const newMember ={
+      id:nanoid(Number),
+      name:newTitle,
+      price:newPrice,
+      quantity:newQuantity
+    }
+    if(newMember.name ==='' || newMember.price ==='' ||newMember.quantity===''){
+      alert('nhap lai'); }
+     else{
+      setDatas(prevDatas=>prevDatas.concat(newMember));}
+      setNewTitle('');
+  }
+    return(
+      <div className="todo-wrapper">
+            <div className="todo-input">
+              <form onSubmit={handleSubmit}>
+              <div>
+                <label >Name</label>
+                <input type="text" name="name" className="todo-input-item" value={newTitle}
+               onChange={(e)=>setNewTitle(e.target.value)}/>
+              </div>
+              <div>
+              <label >price</label>
+                <input type="number" name="price" className="todo-input-item" value={newPrice}
+                onChange={(e)=>setNewPrice(e.target.value)}/>
+              </div>
+              <div>
+              <label >quantity</label>
+                <input type="number" name="quantity" className="todo-input-item" value={newQuantity}
+                onChange={(e)=>setNewQuantity(e.target.value)}/>
+              </div><br/>
+              <button className="btn-info">submit</button>
+              </form>
+            </div>
+      </div>
+    )
+  }
+  export default AddProductForm;
--- src/EditProductFrom.js
+++ src/EditProductFrom.js
@@ -1,50 +1,23 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-    function EditProductForm(){
-      const {id} = useParams();
-      const [data,setData ] = useState([]);
-      const navigate =useNavigate();
-      useEffect(()=>{
-        axios.get('https://dev.crush.asia/coffee-core/guest/category/getListProductGroupingByCategory?tenantId=1&fbclid=IwAR0crUmZPW46nxp_-k9yy2gm4i-4Gg3LioiVwMjMpQx5qxZvTF-w6FB-Q-4'+id)
-        .then(res=>setData(res.data))
-        .catch(err=>console.log(err))
-      })
-      function handleSubmit(e){
-        e.preventDefault();
-        axios.put('https://dev.crush.asia/coffee-core/guest/category/getListProductGroupingByCategory?tenantId=1&fbclid=IwAR0crUmZPW46nxp_-k9yy2gm4i-4Gg3LioiVwMjMpQx5qxZvTF-w6FB-Q-4')
-        .then(res=>{
-          alert("data update successfully");
-          navigate("/")
-        })
-      }
-    return(
-      <div className="todo-wrapper">
-            <div className="todo-input">
-              <form onSubmit={handleSubmit} >
-              <div>
-                <label >Name</label>
-                <input type="text" name="name" className="todo-input-item"
-                value={data.name}
-                onChange={e=>setData({...data,name:e.target.value})}/>
-              </div>
-              <div>
-              <label >price</label>
-                <input type="number" name="price" className="todo-input-item"
-                value={data.price} 
-                onChange={e=>setData({...data,price:e.target.value})}/>
-              </div>
-              <div>
-              <label >quantity</label>
-                <input type="number" name="quantity" className="todo-input-item"
-                 value={data.quantity}
-                 onChange={e=>setData({...data,quantity:e.target.value})}/>
-              </div><br/>
-              <button className="btn-info">update</button>
-              </form>
-            </div>
-      </div>
-    )
-  }
+import React from "react";
+function EditProductForm({product,datas,setDatas}){
+  function handleValue(e){
+    const name = e.target.name;
+    const nameValue = e.target.value;
+    const price = e.target.price;
+    const priceValue = e.target.value;
+    const quantity = e.target.quantity;
+    const quantityValue = e.target.value;
+    const update = datas.map((d)=>d.id===product.id?{...d,[name]:nameValue,[price]:priceValue,[quantity]:quantityValue}:d);
+    setDatas(update);
+  };
+  return(
+    <tr>
+      <td value={product.id}></td>
+     <td> <input type='text' onChange={handleValue} value={product.name} name='name'placeholder='name' /></td>
+     <td><input type='number' onChange={handleValue} value={product.price} name='price' placeholder='price'/></td>
+     <td> <input type='number'  onChange={handleValue}  value={product.quantity} name='quantity' placeholder='quantity'/></td>
+     <td ><button type='submit'>Update</button></td>
+    </tr>
+  )
+}
   export default EditProductForm;
