5dfcffc8f4294cf4fd1c74d3e6a45ea34ece93ff
pnttai <139779096+pnttai@users.noreply.github.com>
2023-08-09 20:35:24 +0700

    Add files via upload

--- EditProductFrom.js
+++ EditProductFrom.js
@@ -1,5 +1,5 @@
 import React from "react";
-function EditProductForm({product,datas,setDatas}){
+function EditProductForm({product,data,setData,setUpdateState,updateState}){
   function handleValue(e){
     const name = e.target.name;
     const nameValue = e.target.value;
@@ -7,16 +7,30 @@ function EditProductForm({product,datas,setDatas}){
     const priceValue = e.target.value;
     const quantity = e.target.quantity;
     const quantityValue = e.target.value;
-    const update = datas.map((d)=>d.id===product.id?{...d,[name]:nameValue,[price]:priceValue,[quantity]:quantityValue}:d);
-    setDatas(update);
+    const update = data.map((d)=>d.id===product.id?{...d,[name]:nameValue,[price]:priceValue,[quantity]:quantityValue}:d);
+    setData(update);
   };
+  function handleUpdate (e){  
+    e.preventDefault()
+    const name = e.target.elements.name.value;
+    const price = e.target.elements.price.value;
+    const quantity = e.target.elements.quantity.value;
+    const update= data.map(d=>d.id===updateState ? {...d,name:name,price:price,quantity:quantity}:d)
+   
+    if(name ==='' || price==='' ||quantity===''){
+      alert('nhập đẩy đủ thông tin cần sửa');
+     }
+     else{
+    setUpdateState(-1)
+    setData(update);  }
+  }
   return(
-    <tr>
+    <tr >
       <td value={product.id}></td>
      <td> <input type='text' onChange={handleValue} value={product.name} name='name'placeholder='name' /></td>
      <td><input type='number' onChange={handleValue} value={product.price} name='price' placeholder='price'/></td>
      <td> <input type='number'  onChange={handleValue}  value={product.quantity} name='quantity' placeholder='quantity'/></td>
-     <td ><button type='submit'>Update</button></td>
+     <td ><button type='submit' onSubmit={handleUpdate}>Update</button></td>
     </tr>
   )
 }
